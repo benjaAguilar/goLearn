5f90bb26fdce7f941f8d4bda42ccfb4191223986
Benja Aguilar <benjaaguilar02@gmail.com>
2026-05-03 20:04:42 -0300

    test: add test coverage for lang feature

--- helloWorld/hello_test.go
+++ helloWorld/hello_test.go
@@ -14,14 +14,44 @@ func assertCorrectMessage(t testing.TB, got, expect string) {
 
 func TestSayHello(t *testing.T) {
 	t.Run("It returns Hello World! if no param is given", func(t *testing.T) {
-		got := SayHello("")
+		got := SayHello("", "")
 		expect := "Hello World!"
 		assertCorrectMessage(t, got, expect)
 	})
 	t.Run("It returns Hello <string> if a param is given", func(t *testing.T) {
-		got := SayHello("Rick!")
+		got := SayHello("Rick!", "")
 		expect := "Hello Rick!"
 
+		assertCorrectMessage(t, got, expect)
+	})
+	t.Run("It can handle spanish languaje", func(t *testing.T) {
+		got := SayHello("", "spanish")
+		expect := "Hola Mundo!"
+		assertCorrectMessage(t, got, expect)
+	})
+	t.Run("It can handle french languaje", func(t *testing.T) {
+		got := SayHello("", "french")
+		expect := "Bonjour Monde!"
+		assertCorrectMessage(t, got, expect)
+	})
+	t.Run("It can handle portuguese languaje", func(t *testing.T) {
+		got := SayHello("", "portuguese")
+		expect := "Olá Mundo!"
+		assertCorrectMessage(t, got, expect)
+	})
+	t.Run("It defaults to english when lang does not match", func(t *testing.T) {
+		got := SayHello("Patrick", "")
+		expect := "Hello Patrick"
+		assertCorrectMessage(t, got, expect)
+	})
+	t.Run("It defaults to english when lang does not exist", func(t *testing.T) {
+		got := SayHello("", "chinese")
+		expect := "Hello World!"
+		assertCorrectMessage(t, got, expect)
+	})
+	t.Run("Can handle case sensitive", func(t *testing.T) {
+		got := SayHello("", "SpaNISH")
+		expect := "Hola Mundo!"
 		assertCorrectMessage(t, got, expect)
 	})
 }
