e5b71ced6fdffbad701e7b990087d3ab7bd7dd2b
Benja Aguilar <benjaaguilar02@gmail.com>
2026-05-09 15:16:20 -0300

    feat: complete integers chapter

---
+++ integers/integers_test.go
@@ -0,0 +1,23 @@
+package integers
+
+import (
+	"testing"
+)
+
+func assertCorrectMessage(t testing.TB, got, expect int) {
+	t.Helper()
+	if got != expect {
+		t.Errorf(
+			"- got: %v \n - expected: %v",
+			got, expect,
+		)
+	}
+}
+
+func TestAddFunction(t *testing.T) {
+	t.Run("Should sum two numbers", func(t *testing.T) {
+		got := Add(3, 3)
+		expect := 6
+		assertCorrectMessage(t, got, expect)
+	})
+}
